6a99fc0e60f8bc47ec6ee66cfbda332bc028cda3
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-16 15:31:11 +0400

    add frr config

--- internal/frr_templ.go
+++ internal/frr_templ.go
@@ -30,7 +30,7 @@ var (
 )
 
 func loadTmpl() *template.Template {
-	tmpl, err := template.New("").ParseFiles(*tmplPath)
+	tmpl, err := template.New(*tmplPath).ParseFiles(*tmplPath)
 	if err != nil {
 		panic(err)
 	}
