3c4fa2f2469bf0aaff937344588c0475f2a6ad62
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-21 17:19:37 +0400

    fix postinstall scripts

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -3,6 +3,6 @@
 systemctl stop oriond
 systemctl enable --now oriond
 
-echo -e "\t\t ***WARNING***"
-echo -e "\t\t This Orion package needs to have authentication certificates installed."
-echo -e "\t\t ***WARNING***"
+echo "***WARNING***"
+echo "This Orion package needs to have authentication certificates installed."
+echo "***WARNING***"
