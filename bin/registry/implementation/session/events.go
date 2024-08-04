e1205f10ab26d74f3f11cc283a2b46d4ee87cd70
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-08-04 14:52:16 +0400

    ignore events when the session's meta is nil

--- bin/registry/implementation/session/events.go
+++ bin/registry/implementation/session/events.go
@@ -29,69 +29,77 @@ func (c *Session) eventListeners() {
 		select {
 		// Handling the events from the newClients stream
 		case newClient := <-newClient.Ch():
-			// When a new client joins, we send a notification message
-			log.Debug().
-				Uint32("new-member-id", newClient.PeerId).
-				Uint32("session", c.meta.memberId).
-				Msgf("notifying of new client")
-
-			c.streamSend.Broadcast(&proto.RPCServerEvent{
-				Event: &proto.RPCServerEvent_NewMember{
-					NewMember: newClient,
-				},
-			})
-		// Handling the events from the invitation stream
-		case invitation := <-c.invitations:
-			if invitation.DestinationPeerId == c.meta.memberId {
+			if c.meta != nil {
+				// When a new client joins, we send a notification message
 				log.Debug().
-					Uint32("src-member-id", invitation.SourcePeerId).
-					Uint32("dst-member-id", invitation.DestinationPeerId).
-					Msg("notifying of new session invitation")
+					Uint32("new-member-id", newClient.PeerId).
+					Uint32("session", c.meta.memberId).
+					Msgf("notifying of new client")
 
 				c.streamSend.Broadcast(&proto.RPCServerEvent{
-					Event: &proto.RPCServerEvent_MemberConnect{
-						MemberConnect: invitation,
+					Event: &proto.RPCServerEvent_NewMember{
+						NewMember: newClient,
 					},
 				})
-			} else {
-				log.Error().
-					Uint32("src-member-id", invitation.SourcePeerId).
-					Uint32("dst-member-id", invitation.DestinationPeerId).
-					Uint32("routine-member-id", c.meta.memberId).
-					Msg("wrong dst-id for this routine")
+			}
+		// Handling the events from the invitation stream
+		case invitation := <-c.invitations:
+			if c.meta != nil {
+				if invitation.DestinationPeerId == c.meta.memberId {
+					log.Debug().
+						Uint32("src-member-id", invitation.SourcePeerId).
+						Uint32("dst-member-id", invitation.DestinationPeerId).
+						Msg("notifying of new session invitation")
+
+					c.streamSend.Broadcast(&proto.RPCServerEvent{
+						Event: &proto.RPCServerEvent_MemberConnect{
+							MemberConnect: invitation,
+						},
+					})
+				} else {
+					log.Error().
+						Uint32("src-member-id", invitation.SourcePeerId).
+						Uint32("dst-member-id", invitation.DestinationPeerId).
+						Uint32("routine-member-id", c.meta.memberId).
+						Msg("wrong dst-id for this routine")
+				}
 			}
 		// Handling the events from the invitation responses
 		case invitation_response := <-c.invitationsResponses:
-			if invitation_response.DestinationPeerId == c.meta.memberId {
-				log.Debug().
-					Uint32("src-member-id", invitation_response.SourcePeerId).
-					Uint32("dst-member-id", c.meta.memberId).
-					Msg("notifying of new invitation request")
+			if c.meta != nil {
+				if invitation_response.DestinationPeerId == c.meta.memberId {
+					log.Debug().
+						Uint32("src-member-id", invitation_response.SourcePeerId).
+						Uint32("dst-member-id", c.meta.memberId).
+						Msg("notifying of new invitation request")
 
-				c.streamSend.Broadcast(&proto.RPCServerEvent{
-					Event: &proto.RPCServerEvent_MemberConnectResponse{
-						MemberConnectResponse: invitation_response,
-					},
-				})
-			} else {
-				log.Error().
-					Uint32("src-member-id", invitation_response.SourcePeerId).
-					Uint32("dst-member-id", invitation_response.DestinationPeerId).
-					Uint32("routine-member-id", c.meta.memberId).
-					Msg("wrong dst-id for this routine")
+					c.streamSend.Broadcast(&proto.RPCServerEvent{
+						Event: &proto.RPCServerEvent_MemberConnectResponse{
+							MemberConnectResponse: invitation_response,
+						},
+					})
+				} else {
+					log.Error().
+						Uint32("src-member-id", invitation_response.SourcePeerId).
+						Uint32("dst-member-id", invitation_response.DestinationPeerId).
+						Uint32("routine-member-id", c.meta.memberId).
+						Msg("wrong dst-id for this routine")
+				}
 			}
 
 		case disposed := <-disposedClient.Ch():
-			log.Debug().
-				Uint32("disposed-member-id", disposed.PeerId).
-				Uint32("member-id", c.meta.memberId).
-				Msg("disposing client")
+			if c.meta != nil {
+				log.Debug().
+					Uint32("disposed-member-id", disposed.PeerId).
+					Uint32("member-id", c.meta.memberId).
+					Msg("disposing client")
 
-			c.streamSend.Broadcast(&proto.RPCServerEvent{
-				Event: &proto.RPCServerEvent_DisconnectedMember{
-					DisconnectedMember: disposed,
-				},
-			})
+				c.streamSend.Broadcast(&proto.RPCServerEvent{
+					Event: &proto.RPCServerEvent_DisconnectedMember{
+						DisconnectedMember: disposed,
+					},
+				})
+			}
 		case <-c.ctx.Done():
 			return
 		}
