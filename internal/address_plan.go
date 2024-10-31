1242d2bcac13665b7446d957806c11091ed7da82
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-10-31 16:30:25 +0400

    change the addresses to not be link local
    Signed-off-by: Matthieu Pignolet <matthieu@matthieu-dev.xyz>

--- internal/address_plan.go
+++ internal/address_plan.go
@@ -7,8 +7,8 @@ import (
 
 func GetSelfAddress(self uint32, other uint32) (*net.IPNet, *net.IPNet, error) {
 	mask := net.CIDRMask(64, 128)
-	selfAddress := net.ParseIP(fmt.Sprintf("fe80::cafe:ffff:%d", self))
-	otherAddress := net.ParseIP(fmt.Sprintf("fe80::cafe:ffff:%d", other))
+	selfAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe:ffff:%d", self))
+	otherAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe:ffff:%d", other))
 
 	return &net.IPNet{
 			IP:   selfAddress,
