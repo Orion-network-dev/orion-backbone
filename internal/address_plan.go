f8257210d64af0c11b10d9793278b76977f2a5f9
Matthieu Pignolet <matthieu@puffer.fish>
2024-11-21 19:53:51 +0400

    change adressing plan to not use link-local addresses

--- internal/address_plan.go
+++ internal/address_plan.go
@@ -3,16 +3,12 @@ package internal
 import (
 	"fmt"
 	"net"
-	"slices"
 )
 
 func GetSelfAddress(self uint32, other uint32) (*net.IPNet, *net.IPNet, error) {
-	mask := net.CIDRMask(120, 128)
-	nid := []uint32{self, other}
-	slices.Sort(nid)
-
-	selfAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe::%d:%d:%d", nid[0], nid[1], self))
-	otherAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe::%d:%d:%d", nid[0], nid[1], other))
+	mask := net.CIDRMask(64, 128)
+	selfAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe:ffff:%d", self))
+	otherAddress := net.ParseIP(fmt.Sprintf("fe80:babe::cafe:ffff:%d", other))
 
 	return &net.IPNet{
 			IP:   selfAddress,
